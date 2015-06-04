9f58771f53a819c66e0694a0ef2ef3638d532524
Angela Barbosa <anpibarcel@yahoo.es>
2015-06-04 11:05:21 -0500

    estos son los ejercicios de los videos de php

---
+++ creaciondeaplicacionesweb/formulariologin.php
@@ -0,0 +1,26 @@
+<html>
+    <body>
+        <table border=1 width="100%">
+        <tr> 
+           <td>
+    	<form action="login.php" method="post">
+    	<input type="text" name="usuario" value="Introduce tu nombre" width=50%>
+    	  	</td>
+    	  	</tr>
+    	    <tr>
+    	    <td>
+    	    
+
+    	<input type="text" name="contrasena" value="introduce tu contrasena" width=50%>
+        </td>
+           </tr>
+           <tr>
+           <td>
+    	<input type="submit">
+    	   </tr>
+    	</form>
+        </table>
+        Si no eres usuario todavia? pulsa<a href="formularioaltausuario.php">AQUI</a>
+    </body>
+
+</html>
---
+++ creaciondeaplicacionesweb/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+/*$_SESSION['usuario'] = "jocarsa";
+$_SESSION['contrasena'] = "jocarsa";
+*/
+echo'
+<html>
+  <head>
+       <meta http-equiv="REFRESH" content = "0;url=formulariologin.php">
+   </head>
+</html>
+';
+?>
